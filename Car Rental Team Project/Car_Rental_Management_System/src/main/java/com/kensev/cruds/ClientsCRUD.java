65d12744ec21a60e020325311ba2493002e96683
Kenan <kensev2000@gmail.com>
2021-06-10 13:34:05 +0300

    ClientsControllerServlet + JSP setup for Client CRUD Operations

--- Car Rental Team Project/Car_Rental_Management_System/src/main/java/com/kensev/cruds/ClientsCRUD.java
+++ Car Rental Team Project/Car_Rental_Management_System/src/main/java/com/kensev/cruds/ClientsCRUD.java
@@ -10,13 +10,18 @@ import java.util.List;
 
 import com.kensev.connection.ConnectionPool;
 import com.kensev.entitites.Clients;
+import com.kensev.entitites.Vehicle;
 
 public class ClientsCRUD {
 	public List<Clients> getAllClients() throws SQLException, InterruptedException {
+		return getAllClients("ID");
+	}
+	
+	public List<Clients> getAllClients(String orderBy) throws SQLException, InterruptedException {
 		List<Clients> allClients = new ArrayList<>(50);
 		try (Connection connection = ConnectionPool.getConnection();
 				Statement prepStatement = connection.createStatement();
-				ResultSet resultSet = prepStatement.executeQuery("SELECT * FROM FN71947.CLIENTS");) {
+				ResultSet resultSet = prepStatement.executeQuery("SELECT * FROM CLIENTS ORDER BY " + orderBy + " limit 100;");) {
 			while (resultSet.next()) {
 				allClients.add(new Clients(resultSet.getString("ID"), resultSet.getString("FIRST_NAME"),
 						resultSet.getString("LAST_NAME"), resultSet.getString("EMAIL"), resultSet.getString("PHONE"),
@@ -30,15 +35,15 @@ public class ClientsCRUD {
 		try (Connection connection = ConnectionPool.getConnection();
 				Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
 						ResultSet.CONCUR_UPDATABLE);) {
-			try (ResultSet rs = statement.executeQuery("SELECT * FROM FN71947.CLIENTS WHERE 1<>1");) {
+			try (ResultSet rs = statement.executeQuery("SELECT * FROM CLIENTS WHERE 1<>1");) {
 				rs.moveToInsertRow();
-				rs.updateString("ID", newClient.getId());
-				rs.updateString("FIRST_NAME", newClient.getFirstName());
-				rs.updateString("LAST_NAME", newClient.getLastName());
+				rs.updateString("ID", newClient.getID());
+				rs.updateString("FIRST_NAME", newClient.getFirst_name());
+				rs.updateString("LAST_NAME", newClient.getLast_name());
 				rs.updateString("EMAIL", newClient.getEmail());
 				rs.updateString("PHONE", newClient.getPhone());
 				rs.updateDate("BIRTHDAY", newClient.getBirthday());
-				rs.updateString("DRIVER_LIC", newClient.getDriverLic());
+				rs.updateString("DRIVER_LIC", newClient.getDriver_lic());
 				rs.insertRow();
 			}
 		}
@@ -47,7 +52,7 @@ public class ClientsCRUD {
 	public void removeClient(String clientId) throws SQLException, InterruptedException {
 		try (Connection connection = ConnectionPool.getConnection();
 				PreparedStatement prepStatement = connection.prepareStatement(
-						"SELECT * FROM FN71947.CLIENTS WHERE ID = ?", ResultSet.TYPE_SCROLL_SENSITIVE,
+						"SELECT * FROM CLIENTS WHERE ID = ?", ResultSet.TYPE_SCROLL_SENSITIVE,
 						ResultSet.CONCUR_UPDATABLE)) {
 			prepStatement.setString(1, clientId);
 			try (ResultSet rs = prepStatement.executeQuery()) {
@@ -61,18 +66,18 @@ public class ClientsCRUD {
 	public void updateClient(Clients client) throws SQLException, InterruptedException {
 
 		try (Connection connection = ConnectionPool.getConnection();
-				PreparedStatement statement = connection.prepareStatement("SELECT * FROM FN71947.CLIENTS WHERE ID = ?",
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM CLIENTS WHERE ID = ?",
 						ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);) {
-			statement.setString(1, client.getId());
+			statement.setString(1, client.getID());
 			try (ResultSet rs = statement.executeQuery();) {
 				if (rs.next()) {
-					rs.updateString("ID", client.getId());
-					rs.updateString("FIRST_NAME", client.getFirstName());
-					rs.updateString("LAST_NAME", client.getLastName());
+					rs.updateString("ID", client.getID());
+					rs.updateString("FIRST_NAME", client.getFirst_name());
+					rs.updateString("LAST_NAME", client.getLast_name());
 					rs.updateString("EMAIL", client.getEmail());
 					rs.updateString("PHONE", client.getPhone());
 					rs.updateDate("BIRTHDAY", client.getBirthday());
-					rs.updateString("DRIVER_LIC", client.getDriverLic());
+					rs.updateString("DRIVER_LIC", client.getDriver_lic());
 					rs.updateRow();
 				}
 			}
@@ -83,7 +88,7 @@ public class ClientsCRUD {
 		Clients foundClient = null;
 		try (Connection connection = ConnectionPool.getConnection();
 				PreparedStatement prepStatement = connection
-						.prepareStatement("SELECT * FROM FN71947.CLIENTS WHERE ID = ?")) {
+						.prepareStatement("SELECT * FROM CLIENTS WHERE ID = ?")) {
 			prepStatement.setString(1, clientId);
 			try (ResultSet resultSet = prepStatement.executeQuery()) {
 				if (resultSet.next()) {
--- Car Rental Team Project/Car_Rental_Management_System/src/main/java/com/kensev/entitites/Clients.java
+++ Car Rental Team Project/Car_Rental_Management_System/src/main/java/com/kensev/entitites/Clients.java
@@ -4,85 +4,66 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 public class Clients {
-	private String Id;
-	private String firstName;
-	private String lastName;
+	private String ID;
+	private String first_name;
+	private String last_name;
 	private String email;
 	private String phone;
 	private Date birthday;
-	private String driverLic;
-
-	public Clients(String id, String firstName, String lastName, String email, String phone, Date birthday,
-			String driverLic) {
-		this.Id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
+	private String driver_lic;
+	public Clients(String iD, String first_name, String last_name, String email, String phone, Date birthday,
+			String driver_lic) {
+		super();
+		ID = iD;
+		this.first_name = first_name;
+		this.last_name = last_name;
 		this.email = email;
 		this.phone = phone;
 		this.birthday = birthday;
-		this.driverLic = driverLic;
+		this.driver_lic = driver_lic;
 	}
-
-	public String getId() {
-		return Id;
+	public String getID() {
+		return ID;
 	}
-
-	public void setId(String id) {
-		Id = id;
+	public void setID(String iD) {
+		ID = iD;
 	}
-
-	public String getFirstName() {
-		return firstName;
+	public String getFirst_name() {
+		return first_name;
 	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
 	}
-
-	public String getLastName() {
-		return lastName;
+	public String getLast_name() {
+		return last_name;
 	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	public Date getBirthday() {
 		return birthday;
 	}
-
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
-
-	public String getDriverLic() {
-		return driverLic;
+	public String getDriver_lic() {
+		return driver_lic;
+	}
+	public void setDriver_lic(String driver_lic) {
+		this.driver_lic = driver_lic;
 	}
 
-	public void setDriverLic(String driverLic) {
-		this.driverLic = driverLic;
-	}
-
-	@Override
-	public String toString() {
-		return "Clients [Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", phone=" + phone + ", birthday=" + birthday + ", driverLic=" + driverLic + "]";
-	}
-
+	
 }
